5b0db37a70ad1eebcae23d035a00441cc005c2f8
tcmalware <genarodvo@gmail.com>
2022-09-22 21:31:47 -0500

    first commit

---
+++ Semana05/ListaCategoria.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Business;
+using Entity;
+
+namespace Semana05
+{
+    /// <summary>
+    /// Lógica de interacción para ListaCategoria.xaml
+    /// </summary>
+    public partial class ListaCategoria : Window
+    {
+        public ListaCategoria()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConsultar_Click(object sender, RoutedEventArgs e)
+        {
+            Cargar();
+        }
+
+        private void Cargar()
+        {
+            BCategoria Bcategoria = null;
+            try
+            {
+                Bcategoria = new BCategoria();
+                dgvCategoria.ItemsSource = Bcategoria.Listar(0);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Comunicarse con el Administrador");
+            }
+            finally
+            {
+                Bcategoria = null;
+            }
+        }
+
+        private void BtnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            ManCategoria manCategoria = new ManCategoria(0);
+            manCategoria.ShowDialog();
+            Cargar();
+        }
+
+        private void DgvCategoria_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int idCategoria;
+            var item = (Categoria)dgvCategoria.SelectedItem;
+            if (null == item) return;
+            idCategoria = Convert.ToInt32(item.IdCategoria);
+
+            ManCategoria manCategoria = new ManCategoria(idCategoria);
+            manCategoria.ShowDialog();
+            Cargar();
+        }
+    }
+}
